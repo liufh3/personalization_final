b18e6cf4cd7845decbca8ec291ab116b8d0ba6d9
DH <asdfdh51@126.com>
2019-12-13 20:32:55 -0500

    util and metrics py file

---
+++ metrics.py
@@ -0,0 +1,48 @@
+import pandas as pd
+import numpy as np
+import json
+from tqdm import tqdm_notebook
+import collections
+import time
+import bisect
+
+
+def mae(true, pred):
+    '''
+    true: as a vector
+    pred: as a vector
+    calculating mean absolute error
+    '''
+    assert len(true) == len(pred)
+    return np.sum(np.abs(true - pred))
+
+
+def mse(true, pred):
+    '''
+    true as a vector
+    pred as a vector
+    calculating mean square error
+    '''
+    assert len(true) == len(pred)
+    return np.sum(np.abs(true - pred) * np.abs(true - pred))
+
+
+def get_rank(sum_dict, arraylike):
+    score = arraylike['rating']
+    user = arraylike['user_id']
+    return sum_dict[user][int(score + 0.5) - 1]
+
+
+def get_rank_pred(sum_dict, arraylike):
+    score = arraylike['prediction']
+    user = arraylike['user_id']
+    return sum_dict[user][int(score + 0.5) - 1]
+
+
+def rank_accuracy(sum_dict, prediction):
+    '''
+    input prediction dataframe and make user_id as index
+    '''
+    rank_true = prediction.apply(lambda x: get_rank(sum_dict, x), axis=1)
+    rank_pred = prediction.apply(lambda x: get_rank_pred(sum_dict, x), axis=1)
+    return np.corrcoef(rank_true, rank_pred)[0][1]
---
+++ util.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import numpy as np
+import json
+from tqdm import tqdm_notebook
+import collections
+import time
+import bisect
+
+def load_json(file_name, save = True):
+    lines = open("yelp_dataset/"+str(file_name)+".json").readlines()
+    keys = list(json.loads(lines[0]).keys())
+    content_dict = collections.defaultdict(list)
+    for line in tqdm_notebook(lines):
+        blob = json.loads(line)
+        for k in keys:
+            content_dict[k].append(blob[k])
+    df = pd.DataFrame(content_dict)
+    if save:
+        df.to_csv(file_name+'.csv',index = 0)
+    return df
+
+
+def get_sum_count(train_df):
+    count_dict = {}
+    count_dict = dict(train_df[['user_id','stars']].groupby('user_id').apply(lambda x:dict(collections.Counter(x['stars']))))
+    sum_dict = collections.defaultdict(list)
+    for user in tqdm_notebook(count_dict.keys()):
+        for star in [1,2,3,4,5]:
+            sum_dict[user].append(count_dict[user].get(star,0))
+        for i in range(4):
+            sum_dict[user][i + 1] += sum_dict[user][i]
